b22ab2f373779c67a51af819ec1faeedc8f2bb14
boronoff <romanticboron@gmail.com>
2023-09-25 11:17:41 +0300

    task 2 is done

--- src/item.py
+++ src/item.py
@@ -1,23 +1,56 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
+
+    Attributes:
+        pay_rate (float): Ставка оплаты для товара (по умолчанию 1.0).
+        all (list): Список всех созданных экземпляров товаров.
     """
+
     pay_rate = 1.0
     all = []
 
-    def __init__(self, name: str, price: float, quantity: int) -> None:
-        if price < 0 or quantity < 0:
+    def __init__(self, name: str, price, quantity) -> None:
+        """
+        Инициализирует экземпляр класса Item.
+
+        Args:
+            name (str): Название товара.
+            price (float): Цена товара.
+            quantity (int): Количество товара.
+
+        Raises:
+            ValueError: Если цена или количество отрицательны.
+        """
+        if Item.string_to_number(price) < 0 or Item.string_to_number(quantity) < 0:
             raise ValueError
-        self.name = name
+        self._name = name
         self.price = price
         self.quantity = quantity
         Item.all.append(self)
+
+    @classmethod
+    def instantiate_from_csv(cls, path):
+        """
+        Создает экземпляры товаров из CSV-файла и добавляет их в список.
+
+        Args:
+            path (str): Путь к CSV-файлу.
+        """
+        with open(path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                cls(row['name'], row['price'], row['quantity'])
 
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
 
-        :return: Общая стоимость товара.
+        Returns:
+            float: Общая стоимость товара.
         """
         return self.price * self.quantity
 
@@ -26,3 +59,39 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price = self.price * Item.pay_rate
+
+    @staticmethod
+    def string_to_number(string: str):
+        """
+        Преобразует строку в число.
+
+        Args:
+            string (str): Строка, представляющая число.
+
+        Returns:
+            int: Преобразованное целое число.
+        """
+        return int(float(string))
+
+    @property
+    def name(self):
+        """
+        Получает название товара.
+
+        Returns:
+            str: Название товара.
+        """
+        return self._name
+
+    @name.setter
+    def name(self, new_name):
+        """
+        Устанавливает название товара.
+
+        Args:
+            new_name (str): Новое название товара (не более 10 символов).
+        """
+        if len(new_name) <= 10:
+            self._name = new_name
+        else:
+            self._name = new_name[:10]
--- tests/test_item.py
+++ tests/test_item.py
@@ -5,31 +5,50 @@ from src.item import Item
 class TestItem:
 
     def test_item_added_to_all(self):
-        # Создаем экземпляр товара
+        """
+        Тест проверяет, что созданный товар добавлен в список 'all' класса Item.
+        """
         item = Item("Товар 4", 40.0, 4)
 
-        # Проверяем, что созданный товар добавлен в список all
         assert item in Item.all
 
     def test_calculate_total_price(self):
-        # Создаем экземпляр товара
+        """
+        Тест проверяет, что метод calculate_total_price возвращает правильное значение.
+        """
         item = Item("Товар 1", 10.0, 2)
 
-        # Проверяем, что метод calculate_total_price возвращает правильное значение
         assert item.calculate_total_price() == 20.0
 
     def test_apply_discount(self):
-        # Создаем экземпляр товара
+        """
+        Тест проверяет, что метод apply_discount правильно применяет скидку к товару.
+        """
         item = Item("Товар 2", 20.0, 3)
 
-        # Применяем скидку
         Item.pay_rate = 0.8
         item.apply_discount()
 
-        # Проверяем, что цена товара изменена в соответствии со скидкой
         assert item.price == 20.0 * Item.pay_rate
 
     def test_quantity_less_than_zero(self):
-        # Попытка создать товар с отрицательным количеством
+        """
+        Тест проверяет, что создание товара с отрицательным количеством вызывает ValueError.
+        """
         with pytest.raises(ValueError):
             Item("Товар 3", 30.0, -1)
+
+    def test_string_to_number(self):
+        """
+        Тест проверяет, что метод string_to_number корректно преобразует строку в число.
+        """
+        assert Item.string_to_number('10.5') == 10
+        assert Item.string_to_number('20.7') == 20
+        assert Item.string_to_number('5.0') == 5
+
+    def test_instantiate_from_csv(self):
+        """
+        Тест проверяет, что метод instantiate_from_csv корректно создает экземпляры товаров из CSV-файла.
+        """
+        Item.instantiate_from_csv("./src/items.csv")
+        assert len(Item.all) == 8
