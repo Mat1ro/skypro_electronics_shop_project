baf1bd69832568b226ff66459ead59091c0e217e
boronoff <romanticboron@gmail.com>
2024-01-26 12:27:58 +0300

    homework 5, again)

--- src/keyboard.py
+++ src/keyboard.py
@@ -4,15 +4,24 @@ from src.item import Item
 class MixinLog:
     def __init__(self, name: int, price: float, quantity: int, language: str) -> None:
         super().__init__(name, price, quantity)
-        self.language = language
+        self._language = language
+
+    @property
+    def language(self) -> str:
+        return self._language
+
+    @language.setter
+    def language(self, new_language: str) -> None:
+        self._language = new_language
 
     def change_lang(self):
         if self.language == "EN":
-            self.language = "RU"
+            self._language = "RU"
         else:
-            self.language = "EN"
+            self._language = "EN"
 
 
 class Keyboard(MixinLog, Item):
     def __init__(self, name: int, price: float, quantity: int, language: str = "EN"):
         super().__init__(name, price, quantity, language)
+
