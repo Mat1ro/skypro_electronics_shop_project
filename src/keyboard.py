b5eaaa7b58bb6cae2cdc69f9f50f5c1baf65cc55
boronoff <romanticboron@gmail.com>
2024-01-28 22:56:46 +0300

    homework 5, again)

--- src/keyboard.py
+++ src/keyboard.py
@@ -10,10 +10,6 @@ class MixinLog:
     def language(self) -> str:
         return self._language
 
-    @language.setter
-    def language(self, new_language: str) -> None:
-        self._language = new_language
-
     def change_lang(self):
         if self.language == "EN":
             self._language = "RU"
@@ -24,4 +20,3 @@ class MixinLog:
 class Keyboard(MixinLog, Item):
     def __init__(self, name: int, price: float, quantity: int, language: str = "EN"):
         super().__init__(name, price, quantity, language)
-
